1ccc9091114aacc673ab73e5f3b16f586a049aeb
Chaos <c410185@163.com>
2017-12-17 23:20:25 +0800

    异步请求返回值和作用域

--- js/企业监管信息.user.js
+++ js/企业监管信息.user.js
@@ -1,28 +1,3 @@
-// ==UserScript==
-// @name         企业监管信息
-// @namespace    http://114.215.104.68
-// @version      0.1
-// @description  扩展监管信息页面
-// @author       IPE_he
-// @match        http://114.215.104.68:89/sitemanage/*
-// @grant        none
-// ==/UserScript==
-
-var s = document.getElementsByClassName('recordView');    //定位到‘查看’属性
-//var s1 = s[0].parentElement.parentElement.parentElement;
-//企业列表是页面上第10个表
-var allTbody = document.getElementsByTagName('tbody');
-var s1 = allTbody[9];
-
-// ==UserScript==
-// @name         插入表格
-// @namespace    http://114.215.104.68
-// @version      0.1
-// @description  扩展监管信息页面插入表格
-// @author       IPE_he
-// @match        http://114.215.104.68:89/sitemanage/*
-// @grant        none
-// ==/UserScript==
 
 (function() {
     'use strict';
@@ -30,30 +5,95 @@ var s1 = allTbody[9];
     //企业列表是页面上第10个表
     var allTbody = document.getElementsByTagName('tbody');
     var s1 = allTbody[9];
+    var recordHtml = '<div><span>fdsfsd</span></div>';
+    var newHtml;
+    var htmlList = [];
     function MyinsertRow(tbodyname) {
         for (let i = 1,l = tbodyname.rows.length; i < l; i=i+1) {
             var newRow = tbodyname.insertRow(i*2);
             newRow.style.height = '100px';
             var newCell = newRow.insertCell(0);
             newCell.colSpan = '11';
+            getRviewHtml(s[i-1]);
+            //console.log(i-1);
+            //console.log(htmlList.length);  //显示为0
+            //newCell.innerHTML = newHtml;
         }
     }
+    function formatHtml(oldrViewhtml) {
+    var oldTr = oldrViewhtml.getElementsByTagName('td');
+    //直接取2,3(信用代码);4,5（机构代码）;6,7（地址）;12,13（违法事实）；14,15（处罚手段）；18,19监管记录来源；最后是详情17；获取值，然后填入新的单元格中，详情直接填入innerHTML。试试
+    //建立第一行，6单元格
+    var tab0 = document.createElement('table');
+    var tr0_0 = tab0.insertRow();
+    var cell_0 = tr0_0.insertCell();
+    var cell_1 = tr0_0.insertCell();
+    cell_0.innerText = oldTr[2].innerText;
+    cell_1.innerText = oldTr[3].innerText;
 
-    MyinsertRow(s1);
-    var recordHtml = '<div><span>fdsfsd</span></div>';
+    var cell_2 = tr0_0.insertCell();
+    var cell_3 = tr0_0.insertCell();
+    cell_2.innerText = oldTr[4].innerText;
+    cell_3.innerText = oldTr[5].innerText;
+
+    var cell_4 = tr0_0.insertCell();
+    var cell_5 = tr0_0.insertCell();
+    cell_4.innerText = oldTr[6].innerText;
+    cell_5.innerText = oldTr[7].innerText;
+    //建立第二行，6单元格
+    var tr0_1 = tab0.insertRow();
+    var cell_6 = tr0_1.insertCell();
+    var cell_7 = tr0_1.insertCell();
+    cell_6.innerText = oldTr[12].innerText;
+    cell_7.innerText = oldTr[13].innerText;
+
+    var cell_8 = tr0_1.insertCell();
+    var cell_9 = tr0_1.insertCell();
+    cell_8.innerText = oldTr[14].innerText;
+    cell_9.innerText = oldTr[15].innerText;
+
+    var cell_10 = tr0_1.insertCell();
+    var cell_11 = tr0_1.insertCell();
+    cell_10.innerText = oldTr[18].innerText;
+    cell_11.innerText = oldTr[19].innerText;
+    //建立第二个表格，存放监管详情，为了增加导航条，所以放在div里
+    var div0 = document.createElement('div');
+    var tab1 = document.createElement('table');
+    var tr1_0 = tab1.insertRow();
+    var cell_12 = tr1_0.insertCell();
+    cell_12.innerHTML = oldTr[17].innerHTML;
+    div0.appendChild(tab1);
+    //CSS这个改变不知道对不对
+    //div0.style.cssText = 'height : 200px; overflow-y: scroll;'
+    div0.style.height = '200px';
+    div0.style.overflowY = 'scroll';
+    var tab0_parent = document.createElement('table');
+    var div0_parent = document.createElement('div');
+    tab0_parent.appendChild(tab0);
+    div0_parent.appendChild(div0);
+    return tab0_parent.innerHTML + div0_parent.innerHTML; //因为取的是包含的HTML元素，这样会导致最外层的一个标签作为容器本身的元素而不被传递
+}
+
+
     function getRviewHtml(recordViewVal) {
         var xh = new XMLHttpRequest();
-        xh.open('get', recordViewVal.href);
-        xh.responseType = 'document';
         xh.onreadystatechange = function () {
-                recordHtml = xh.response;
-        }
-        xh.send();
+            if (xh.readyState == 4 && xh.status == 200){
+                var recordHtml = xh.response;
+                var newHtml = formatHtml(recordHtml);
+                htmlList.push(newHtml);
+                //console.log(htmlList.length);此处能获得所有值，但是在外面还是无法获得HTMLlist的长度，感觉还是不对
+                //console.log(newHtml);//将获取链接后的操作都写在此处，可能存在变量作用域问题，在外部无法访问到recordHTML的值,直接搜索此函数的返回值，详细在看看
+            }
+            //return recordHtml;
+        };
+        xh.open('get', recordViewVal.href, true);
+        xh.responseType = 'document';
+        xh.send(null);
     }
-    getRviewHtml(s[0]);
-    if (recordHtml) {
-        console.log(recordHtml);
-    } else {
-        console.log('recordHtml为空');
+    function returnHtml(somehtml){
+        htmlList.push(somehtml);
     }
+    MyinsertRow(s1);
+    //getRviewHtml(s[0]);
 })();
